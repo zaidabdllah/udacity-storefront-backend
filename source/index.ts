7f3611a6085769e44a0c22764da935c8aa0632c3
zaid abdallah <zzidxrh@gmail.com>
2026-04-14 19:19:38 +0300

    setup dot env file and update package.json and source/index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
